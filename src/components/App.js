178ce05029a575b5769d46c8ce3336a8dcc855a6
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-02-15 02:10:21 +0530

    Initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const btnRender = () => {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  };
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+    <>
+      <div id="main">// Do not alter the main div</div>
+      <button id="click" onClick={btnRender}>
+        Click
+      </button>
+    </>
   );
 }
-
 
 export default App;
