5ccf4547aac88eb9e37f25533585c59af3326646
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-02-17 20:35:50 +0530

    First

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,24 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const btnRender = () => {
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
+  const [show, setShow] = useState(false);
+
+  const showPara = () => {
+    setShow((show) => !show);
   };
+
   return (
     <>
       <div id="main">// Do not alter the main div</div>
-      <button id="click" onClick={btnRender}>
+      <button id="click" onClick={showPara}>
         Click
       </button>
+      {show && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </>
   );
 }
